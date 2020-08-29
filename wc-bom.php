3fa5a367f65ba8b55a58da7696abed31dcfe4ec0
Rover <rover@MacBook-Pro.local>
2020-08-29 19:03:06 +0800

    include_once __DIR__ . '/classes/class-wc-bom-settings.php';

--- wc-bom.php
+++ wc-bom.php
@@ -193,9 +193,9 @@ class WC_Bom {
 	 */
 	public function load_classes() {
 		//include_once __DIR__ . '/classes/class-wc-bom-data.php';
-		//include_once __DIR__ . '/classes/class-wc-bom-post.php';
+		include_once __DIR__ . '/classes/class-wc-bom-post.php';
 		//include_once __DIR__ . '/classes/class-wc-bom-settings.php';
-		include_once __DIR__ . '/classes/class-wc-bom-logger.php';
+		//include_once __DIR__ . '/classes/class-wc-bom-logger.php';
 		//include_once __DIR__ . '/includes/bom-fields.php';
 		if ( is_admin() ) {
 			include_once __DIR__ . '/classes/class-wc-bom-settings.php';
