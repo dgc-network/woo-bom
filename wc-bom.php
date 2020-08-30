587e0f69d3de7d773e2b66c432139d3b5b112660
Rover <rover@MacBook-Pro.local>
2020-08-30 20:43:02 +0800

    //$settings = WC_Bom_Settings::getInstance();

--- wc-bom.php
+++ wc-bom.php
@@ -105,7 +105,7 @@ class WC_Bom {
 		add_filter( 'plugin_action_links', [ $this, 'plugin_links' ], 10, 5 );
 
 		//$this->load_classes();
-		$settings = WC_Bom_Settings::getInstance();
+		//$settings = WC_Bom_Settings::getInstance();
 		//$post     = WC_Bom_Post::getInstance();
 
 		//include_once 'uninstall.php';
